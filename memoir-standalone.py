f521ec47e7b3b7ee07c0031cd78f96e3027545b1
brucepro <git@brucepro.net>
2024-07-11 11:48:42 -0700

    main startpoint using api endpoint
    This version will take an api and manage context for the api to allow for the full functions of Memoir+ memory system to work.

---
+++ memoir-standalone.py
@@ -0,0 +1,8 @@
+"""
+memoir-standalone.py - main entrance of the script for the standalone version using a compatiable openai endpoint
+
+Memoir+ a persona extension for Text Gen Web UI. 
+MIT License
+
+Copyright (c) 2024 brucepro
+"""
